6fb335f350b13ecbf910e02e96e8150b32fb0f39
suzandev <dev.suzanchandra@gmail.com>
2023-07-30 08:28:48 +0600

    javaScript start for programming hero

---
+++ script.js
@@ -0,0 +1,49 @@
+// var onionPrice = 20;
+// var eggPrice = 10;
+
+// var totalPrice = onionPrice + eggPrice;
+
+// console.log(totalPrice);
+
+// var priceDifference = onionPrice - eggPrice;
+
+// console.log(priceDifference);
+
+// Multiplication
+// var orangePrice = 20;
+// var quantity = 7;
+
+// var totalCost = orangePrice * quantity;
+// console.log(totalCost);
+
+// division
+
+// var money = 500;
+// var player = 10;
+// var eachPlayer = money / player;
+// console.log(eachPlayer);
+
+// // shortHand
+
+// var money = money + 10;
+
+// money += 10;
+
+// console.log(money);
+
+//  ===== convert string to number =====
+
+// var price1 = "32";
+// var price2 = "10.46";
+
+// var price3 = parseInt(price1);
+// console.log(price3);
+
+// var price4 = parseFloat(price2);
+// console.log(price4);
+
+// ====== array start =======
+
+var friendsAge = [11, 21, 45, 14, 105, 6];
+// akta array er moddho koiti element ache janar jonno .length
+console.log(friendsAge.length);
