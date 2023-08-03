42686e506606207a0fa574b97f5f46fdacf16a26
suzandev <dev.suzanchandra@gmail.com>
2023-08-03 17:53:21 +0600

    just practice some array and object

--- script.js
+++ script.js
@@ -146,3 +146,153 @@
 // } else {
 //   console.log("you failed in your exam");
 // }
+
+// ========loop start ========
+
+// task 1 =  Display "ajke amar mon valo nei" for 39 times
+
+/*
+for (var i = 1; i <= 39; i++) {
+  console.log("ajke amar mon valo nei", i);
+}
+*/
+
+// task 2 Display numbers between 58 to 98
+
+/*
+for (var i = 58; i <= 98; i++) {
+  console.log(i);
+}
+console.log("*****only for even numbers from 412 to 456****");
+*/
+
+// // task 3 = Show all even numbers from 412 to 456
+
+/*
+for (var i = 412; i <= 456; i++) {
+  if (i === 412) {
+    console.log(i + " is even number");
+  } else if (i % 2 === 0) {
+    console.log(i + " is even number");
+  }
+  //   else {
+  //     console.log(i + " is odd number");
+  //   }
+}
+
+console.log("*****only for odd numbers from 581 to 623****");
+*/
+
+// task 4 = Show all odd numbers from 581 to 623
+
+/*
+for (var i = 581; i <= 623; i++) {
+  if (i % 2 != 0) {
+    console.log(i + " is odd number");
+  }
+}
+
+console.log(
+  "*****Declare an array for all the topics that you have learned last few days and display****"
+);
+
+*/
+
+// task 6 = Declare an array for all the topics that you have learned last few days and display then as output
+
+/*
+var myJourney = [
+  "Html3",
+  "Css",
+  "Html5",
+  "Css3",
+  "Tailwind Css",
+  "DaisyUi",
+  "Javascript",
+  "Condition",
+  "String",
+  "Array",
+  "Loop",
+];
+
+for (var i = 0; i < myJourney.length; i++) {
+  var myResult = myJourney[i];
+  console.log(myResult);
+}
+*/
+
+// ======== object start ========
+
+/*
+var shoppingCart = {
+  books: 3,
+  sunglass: 1,
+  keyboard: 5,
+  mouse: 1,
+  pen: 25,
+  shoes: 2,
+  bottle: 1,
+};
+
+const keys = Object.keys(shoppingCart);
+console.log(keys);
+
+const values = Object.values(shoppingCart);
+console.log(values);
+
+for (let i = 0; i < keys.length; i++) {
+  let propertyName = keys[i];
+  let propertyValue = shoppingCart[propertyName];
+  console.log(propertyName, propertyValue);
+}
+
+*/
+
+/*
+
+function isEven(number) {
+  const remainder = number % 2;
+
+  if (remainder === 0) {
+    return console.log("number is even");
+  } else {
+    return console.log("number is odd");
+  }
+
+  // console.log(remainder);
+}
+
+const myNumber = isEven(303);
+console.log(myNumber);
+
+*/
+
+function getSumOfArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const index = i;
+    const element = arr[index];
+    sum = sum + element;
+  }
+  return sum;
+}
+
+function getOddNumbers(numbers) {
+  let oddNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    const index = i;
+    const element = numbers[index];
+    if (element % 2 !== 0) {
+      oddNumbers.push(element);
+    }
+  }
+  return oddNumbers;
+}
+
+const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+
+const collectOddNumbers = getOddNumbers(myNumbers);
+console.log(collectOddNumbers);
+
+const oddNumbersSum = getSumOfArray(collectOddNumbers);
+console.log("total odd numbers sum: ", oddNumbersSum);
