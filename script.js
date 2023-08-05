9bbe838c66a709ad1dc81ac07df47a5b3b3e7161
suzandev <dev.suzanchandra@gmail.com>
2023-08-05 23:11:18 +0600

    add some shopping card total sum

--- script.js
+++ script.js
@@ -639,6 +639,7 @@ console.log("total wood require:", totalWood);
 
 // =*=*=*=*=*=*=*=*= Shopping objects example *=*=*=*=*=*=*=
 
+/*
 const phone = [
   {
     name: "Samsung",
@@ -705,3 +706,39 @@ function cheapestPhone(phones) {
 
 const mySelection = cheapestPhone(phone);
 console.log(mySelection);
+
+*/
+
+// =*=*=*=*=*=*=*=*= Shopping card *=*=*=*=*=*=*=
+
+const shoppingCart = [
+  {
+    name: "shoe",
+    price: 1200,
+  },
+  {
+    name: "shirt",
+    price: 2200,
+  },
+  {
+    name: "pant",
+    price: 3700,
+  },
+  {
+    name: "belt",
+    price: 600,
+  },
+];
+
+function totalCost(products) {
+  let sum = 0;
+
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    sum = sum + product.price;
+  }
+  return sum;
+}
+
+const expense = totalCost(shoppingCart);
+console.log("total expense today:", expense);
