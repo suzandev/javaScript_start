8bd3d2b0a446bd8a7e6ab7566f23f82008dd3bd8
suzandev <dev.suzanchandra@gmail.com>
2023-08-05 23:42:12 +0600

    add some math

--- script.js
+++ script.js
@@ -711,6 +711,7 @@ console.log(mySelection);
 
 // =*=*=*=*=*=*=*=*= Shopping card *=*=*=*=*=*=*=
 
+/*
 const shoppingCart = [
   {
     name: "shoe",
@@ -742,3 +743,47 @@ function totalCost(products) {
 
 const expense = totalCost(shoppingCart);
 console.log("total expense today:", expense);
+
+*/
+
+// =*=*=*=*=*=*=*=*= Discount for shopping *=*=*=*=*=*=*=
+
+/**
+ * if ticket numbers is less than 100, per ticket price: 100
+ * if ticket numbers is more than 100, but less than 200, First 100 tickets will be 100/ticket.
+ * rest tickets will be 90 taka per piece
+ * if you purchase more than 200 tickets,
+ *  first 100 --> 100tk
+ * 101-200 --> 90tk
+ * 200+ --> 70tk
+ */
+
+/*
+function ticketPrice(ticketQuantity) {
+  const first100Rate = 100;
+  const second100Rate = 90;
+  const restTicketRate = 70;
+
+  if (ticketQuantity <= 100) {
+    const price = ticketQuantity * first100Rate;
+    return price;
+  } else if (ticketQuantity <= 200) {
+    const first100price = 100 * first100Rate;
+    const restTicketQuantity = ticketQuantity - 100;
+    const restTicketPrice = restTicketQuantity * second100Rate;
+    const totalPrice = first100price + restTicketPrice;
+    return totalPrice;
+  } else {
+    const first100price = 100 * first100Rate;
+    const second100price = 100 * second100Rate;
+    const restTicketQuantity = ticketQuantity - 200;
+    const restTicketPrice = restTicketQuantity * restTicketRate;
+    const totalPrice = first100price + second100price + restTicketPrice;
+    return totalPrice;
+  }
+}
+
+const price = ticketPrice(230);
+console.log("price:", price);
+
+*/
