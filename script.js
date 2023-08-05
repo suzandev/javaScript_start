be78857b33dedcb74ef2aa85a59518f7d637a39a
suzandev <dev.suzanchandra@gmail.com>
2023-08-05 22:54:59 +0600

    add some phone lowest price

--- script.js
+++ script.js
@@ -636,3 +636,72 @@ const totalWood = woodCalculator(2, 2, 5);
 console.log("total wood require:", totalWood);
 
 */
+
+// =*=*=*=*=*=*=*=*= Shopping objects example *=*=*=*=*=*=*=
+
+const phone = [
+  {
+    name: "Samsung",
+    camera: 12,
+    storage: "32gb",
+    price: 36000,
+    color: "silver",
+  },
+  {
+    name: "Walton",
+    camera: 12,
+    storage: "32gb",
+    price: 22000,
+    color: "silver",
+  },
+  {
+    name: "Iphone",
+    camera: 12,
+    storage: "32gb",
+    price: 82000,
+    color: "silver",
+  },
+  {
+    name: "Xaomi",
+    camera: 12,
+    storage: "32gb",
+    price: 52000,
+    color: "silver",
+  },
+  {
+    name: "Nokia",
+    camera: 12,
+    storage: "32gb",
+    price: 44000,
+    color: "silver",
+  },
+  {
+    name: "Oppo",
+    camera: 12,
+    storage: "32gb",
+    price: 20000,
+    color: "silver",
+  },
+  {
+    name: "HTC",
+    camera: 12,
+    storage: "32gb",
+    price: 62000,
+    color: "silver",
+  },
+];
+
+// sob cheye kom price er phone paoya jabe
+function cheapestPhone(phones) {
+  let cheapest = phones[0];
+  for (let i = 0; i < phones.length; i++) {
+    const phone = phones[i];
+    if (phone.price < cheapest.price) {
+      cheapest = phone;
+    }
+  }
+  return cheapest;
+}
+
+const mySelection = cheapestPhone(phone);
+console.log(mySelection);
